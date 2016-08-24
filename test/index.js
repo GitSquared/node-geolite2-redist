d29a0fa736d545312e54b04fbd44f69920f048ca
Dmitry Shirokov <deadrunk@gmail.com>
2016-08-25 09:58:30 +1000

    Fix test for earlier node versions

--- test/index.js
+++ test/index.js
@@ -2,14 +2,14 @@ const assert = require('assert');
 const fs = require('fs');
 const geolite2 = require('../');
 
-describe('geolite2', () => {
-  it('should return a valid city db path', () => {
+describe('geolite2', function() {
+  it('should return a valid city db path', function() {
     var stat = fs.statSync(geolite2.paths.city);
     assert(stat.size > 1e6);
     assert(stat.ctime);
   });
 
-  it('should return a valid country db path', () => {
+  it('should return a valid country db path', function() {
     var stat = fs.statSync(geolite2.paths.country);
     assert(stat.size > 1e6);
     assert(stat.ctime);
