d36491af3427be27b1c8c37440573fc1cfb4410a
Dmitry Shirokov <dshirokov@seek.com.au>
2020-01-02 10:18:56 +1100

    Support end-user license agreement

--- scripts/postinstall.js
+++ scripts/postinstall.js
@@ -4,10 +4,22 @@ const zlib = require("zlib");
 const path = require("path");
 const tar = require("tar");
 
+const licenseKey = process.env.MAXMIND_LICENSE_KEY;
+if (!licenseKey) {
+  console.error(`Error: License key is not configured.\n
+  You need to signup for a _free_ Maxmind account to get a license key.
+  Go to https://www.maxmind.com/en/geolite2/signup, obtain your key and
+  put it in the MAXMIND_LICENSE_KEY environment variable\n`);
+  process.exit(1);
+}
+
+const link = edition =>
+  `https://download.maxmind.com/app/geoip_download?edition_id=${edition}&license_key=${licenseKey}&suffix=tar.gz`;
+
 const links = [
-  "https://geolite.maxmind.com/download/geoip/database/GeoLite2-City.tar.gz",
-  "https://geolite.maxmind.com/download/geoip/database/GeoLite2-Country.tar.gz",
-  "https://geolite.maxmind.com/download/geoip/database/GeoLite2-ASN.tar.gz"
+  link('GeoLite2-City'),
+  link('GeoLite2-Country'),
+  link('GeoLite2-ASN'),
 ];
 
 const downloadPath = path.join(__dirname, "..", "dbs");
