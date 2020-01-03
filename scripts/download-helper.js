7be98e1f2f09dc25c84e92d953f6452ece175f50
GitSquared <gabriel@saillard.dev>
2020-01-03 18:25:41 +0100

    new redistribution mechanism, updated license

---
+++ scripts/download-helper.js
@@ -0,0 +1,100 @@
+const fs = require('fs');
+const https = require('https');
+const zlib = require('zlib');
+const path = require('path');
+const tar = require('tar');
+const md5sum = require('md5-file/promise');
+
+// TODO: Change URL from fork to @runk's repo
+const link = file => `https://raw.githubusercontent.com/GitSquared/node-geolite2/master/redist/${file}`;
+
+const editions = [
+  { name: 'GeoLite2-ASN' },
+  { name: 'GeoLite2-City' },
+  { name: 'GeoLite2-Country' }
+];
+
+editions.forEach((edition, index) => {
+  editions[index].dbURL = link(edition.name+'.tar.gz');
+  editions[index].checksumURL = link(edition.name+'.mmdb.md5');
+});
+
+function fetchChecksums() {
+  let downloads = [];
+
+  editions.forEach(edition => {
+    downloads.push(new Promise(resolve => {
+      https.get(edition.checksumURL, res => {
+        let checksum = '';
+        res.on('data', chunk => {
+          checksum = checksum+chunk.toString();
+        });
+        res.on('end', () => {
+          if (!res.complete || checksum.length !== 32) throw new Error(`Could not fetch checksum for ${edition.name}`);
+          edition.checksum = checksum;
+          resolve();
+        });
+      });
+    }));
+  });
+
+  return Promise.all(downloads);
+}
+
+function fetchDatabases(outPath) {
+  const fetch = url => new Promise(resolve => {
+     https.get(edition.dbURL, res => {
+        try {
+          resolve(res.pipe(zlib.createGunZip({})));
+        } catch(_) {
+          throw new Error(`Could not fetch ${edition.name}`);
+        }
+      });
+  });
+
+  let downloads = [];
+
+  editions.forEach(edition => {
+    downloads.push(new Promise(resolve => {
+      fetch(edition.dbURL).then(res => {
+        res.pipe(tar.t()).on('entry', entry => {
+          if (entry.path.endsWith('.mmdb')) {
+            const dstFilename = path.join(outPath, path.basename(entry.path));
+            entry.pipe(fs.createWriteStream(dstFilename));
+          }
+        });
+      });
+    }));
+  });
+
+  return Promise.all(downloads);
+}
+
+function verifyAllChecksums(downloadPath) {
+  let promises = [];
+
+  editions.forEach(edition => {
+    promises.push(new Promise((resolve, reject) => {
+      md5sum(path.join(downloadPath, edition.name+'.mmdb.md5')).then(hash => {
+        if (hash === edition.checksum) {
+          resolve();
+        } else {
+          reject(`Mismatched checksums for ${edition.name}`);
+        }
+      }).catch(e => {
+        reject(e);
+      });
+    }));
+  });
+
+  return Promise.all(promises);
+}
+
+module.exports = {
+  fetchChecksums,
+  fetchDatabases,
+  verifyAllChecksums,
+  getEditions: () => {
+    return editions;
+  }
+};
--- scripts/postinstall.js
+++ scripts/postinstall.js
@@ -1,46 +1,16 @@
 const fs = require("fs");
-const https = require("https");
-const zlib = require("zlib");
 const path = require("path");
-const tar = require("tar");
 
-const licenseKey = process.env.MAXMIND_LICENSE_KEY;
-if (!licenseKey) {
-  console.error(`Error: License key is not configured.\n
-  You need to signup for a _free_ Maxmind account to get a license key.
-  Go to https://www.maxmind.com/en/geolite2/signup, obtain your key and
-  put it in the MAXMIND_LICENSE_KEY environment variable\n`);
-  process.exit(1);
-}
+const downloadHelper = require('./download-helper.js');
 
-const link = edition =>
-  `https://download.maxmind.com/app/geoip_download?edition_id=${edition}&license_key=${licenseKey}&suffix=tar.gz`;
-
-const links = [
-  link('GeoLite2-City'),
-  link('GeoLite2-Country'),
-  link('GeoLite2-ASN'),
-];
-
-const downloadPath = path.join(__dirname, "..", "dbs");
-
+const downloadPath = path.join(__dirname, '..', 'dbs');
 if (!fs.existsSync(downloadPath)) fs.mkdirSync(downloadPath);
 
-const download = url =>
-  new Promise(resolve => {
-    https.get(url, function(response) {
-      resolve(response.pipe(zlib.createGunzip({})));
-    });
-  });
-
-console.log("Downloading maxmind databases...");
-links.forEach(url =>
-  download(url).then(result =>
-    result.pipe(tar.t()).on("entry", entry => {
-      if (entry.path.endsWith(".mmdb")) {
-        const dstFilename = path.join(downloadPath, path.basename(entry.path));
-        entry.pipe(fs.createWriteStream(dstFilename));
-      }
-    })
-  )
-);
+console.log("Downloading MaxMind databases from mirror...");
+downloadHelper.fetchChecksums().then(() => {
+  return downloadHelper.fetchDatabases(downloadPath);
+}).then(() => {
+  return downloadHelper.verifyAllChecksums(downloadPath);
+}).catch(e => {
+  throw e;
+});
