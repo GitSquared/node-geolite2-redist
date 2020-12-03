d6d705ceccbbb17a56f9fbf187d6d69babc54fa6
Gabriel Saillard <gabriel@saillard.dev>
2020-12-03 20:23:39 +0100

    feat: (breaking) allow specifying custom db download path #11

--- index.d.ts
+++ index.d.ts
@@ -2,11 +2,7 @@ import { EventEmitter } from "events";
 import { Reader } from "maxmind";
 
 declare module "geolite2-redist" {
-  export const paths: {
-    "GeoLite2-ASN": string;
-    "GeoLite2-City": string;
-    "GeoLite2-Country": string;
-  }
+  export const databases: string[]
   export const open:
     <T>(database: string, readerBuilder: (database: string) =>
       (Reader<T> | Promise<Reader<T>>)) => Promise<Reader<T>>
@@ -14,6 +10,7 @@ declare module "geolite2-redist" {
     public downloading: boolean;
     private _checker: ReturnType<typeof setTimeout>;
 
+    downloadDbs(path?: string): Promise<void>;
     checkUpdates(): Promise<void>;
     update(): void;
     triggerUpdate(): Promise<void>;
